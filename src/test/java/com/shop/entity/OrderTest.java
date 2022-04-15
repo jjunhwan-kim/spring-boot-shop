0e920e64776e35062b79fdf8ee1714885c576da7
JunHwan Kim <nefertiri972@gmail.com>
2022-04-15 22:58:34 +0900

    Add cascade, orphan removal
    - Order 엔티티에 Cascade, Orphan Removal 옵션 추가
    - Cascade는 Order가 변할 때 Order의 연관관계에 있는 엔티티에 전파
    - Orphan Removal은 Order의 필드에서 제거시 연결이 끊어진 엔티티 삭제

---
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -0,0 +1,105 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestPropertySource(locations = "classpath:application-test.properties")
+@Transactional
+@SpringBootTest
+class OrderTest {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    Item createItem() {
+        Item item = new Item();
+        item.setItemName("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("상세설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegisteredTime(LocalDateTime.now());
+        item.setUpdatedTime(LocalDateTime.now());
+        return item;
+    }
+
+    @Test
+    @DisplayName("영속성 전이 테스트")
+    void cascadeTest() {
+        Order order = new Order();
+
+        for (int i = 0; i < 3; i++) {
+            Item item = createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(10000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+
+        orderRepository.saveAndFlush(order);
+        em.clear();
+
+        Order savedOrder = orderRepository.findById(order.getId()).orElseThrow(EntityNotFoundException::new);
+        assertThat(savedOrder.getOrderItems().size()).isEqualTo(3);
+    }
+
+    Order createOrder() {
+        Order order = new Order();
+
+        for (int i = 0; i < 3; i++) {
+            Item item = createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(10000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+
+        Member member = new Member();
+        memberRepository.save(member);
+
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    void orphanRemovalTest() {
+        Order order = createOrder();
+        order.getOrderItems().remove(0);
+        em.flush();
+    }
+}
